7a543472f07dad57c204728dcb6404f02bbb5da2
MozammelHosen <mdmozammelhosen15701@gmail.com>
2023-01-14 09:57:15 +0600

    server add

--- src/App.js
+++ src/App.js
@@ -2,17 +2,54 @@ import { useEffect, useState } from "react";
 import "./App.css";
 
 function App() {
-  const [user, setUser] = useState([]);
+  const [users, setUsers] = useState([]);
 
   useEffect(() => {
     fetch("http://localhost:5000/users")
       .then((res) => res.json())
-      .then((data) => setUser(data));
+      .then((data) => setUsers(data));
   }, []);
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    const name = event.target.name.value;
+    const email = event.target.email.value;
+    const user = { name, email };
+    console.log(user);
+
+    fetch("http://localhost:5000/users", {
+      method: "post",
+      headers: {
+        "content-type": "application/json",
+      },
+      body: JSON.stringify(user),
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        console.log(data);
+        const newUser = [...users, data];
+        setUsers(newUser);
+      })
+      .catch((err) => console.log(err));
+
+    event.target.reset();
+  };
 
   return (
     <div className="App">
-      <h1>Welcome to client site{user.length}</h1>
+      <form onSubmit={handleSubmit}>
+        <input type="text" name="name" id="" placeholder="text" /> <br />
+        <input type="email" name="email" id="" placeholder="email" /> <br />
+        <input type="submit" value="Add User" />
+      </form>
+
+      <h1>Welcome to client site{users.length}</h1>
+      {users.map((user) => (
+        <p key={user.id}>
+          userName:{user.name}
+          userEmail: {user.email}
+        </p>
+      ))}
     </div>
   );
 }
